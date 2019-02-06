13b77da7f114660ddd2315540fa337aee0ae6ffa
Ben Shurts <bshurts93@gmail.com>
2019-02-06 11:50:17 -0600

    Initial file upload with psudo code in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//-----------------// VARIABLES //-----------------//
+
+
+
+
+
+
+//-----------------// FUNCTIONS //-----------------//
+
+
+
+//-----------------// PSUDO CODE //-----------------//
+
+// App generates a randomly selected letter and stored in a variable
+
+
+// User picks letter on keyboard
+
+
+// If it's correct, user wins + win count goes up + game resets
+
+ 
+// If it's wrong, take one guess away + display the wrong letter to the DOM
+
+
+// If winner runs out of guesses and loses, add one loss + reset both game and guess count
+
