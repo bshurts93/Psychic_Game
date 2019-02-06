d090c6a6c95bd372ccadf04a4f576becfc2ede00
Ben Shurts <bshurts93@gmail.com>
2019-02-06 12:31:54 -0600

    Created winning and losing functions to handle user keypresses and trigger the right changes in both JS variables (score and next letter) and their relative DOM elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,50 @@
 //-----------------// VARIABLES //-----------------//
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var randomLetter = getRandomLetter();
+var randomLetter;
+
+// SCORING
+var wins = parseInt(document.getElementById("wins").textContent);
+var losses = parseInt(document.getElementById("losses").textContent);
+
 
 
 
 //-----------------// FUNCTIONS //-----------------//
 function getRandomLetter() {
     var letter = alphabet[Math.floor(Math.random() * 26)];
-    return letter;
+    document.querySelector(".random-letter").innerHTML = letter;
+    randomLetter = letter;
+}
+
+function winner() {
+    wins++;
+    document.getElementById("wins").innerHTML = wins;
+    getRandomLetter();
+}
+
+function loser() {
+    losses++
+    document.getElementById("losses").innerHTML = losses;
 }
 
 
 //-----------------// PSUDO CODE //-----------------//
 
+
 // App generates a randomly selected letter and stored in a variable
+getRandomLetter();
 document.querySelector(".random-letter").innerHTML = randomLetter;
 
 // User picks letter on keyboard
 document.onkeydown = function (e) {
     var keypress = e.key;
     
-
+    if(keypress === randomLetter) {
+        winner();
+    } else {
+        loser();
+    }
 }
 
 // If it's correct, user wins + win count goes up + game resets
